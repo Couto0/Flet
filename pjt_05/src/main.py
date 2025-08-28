3ed3afbdec0d6edc6b439bfec75b91df051d7413
Couto0 <arthurdossantos1201@gmail.com>
2025-08-28 08:57:16 -0300

    initial commit: add main application files, configuration, and README

---
+++ pjt_05/src/main.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    conteiner = ft.Container(
+        content=ft.Text("Meu Container", size=20, weight="bold", theme_style= ft.TextThemeStyle.HEADLINE_MEDIUM),
+        padding=20,
+        margin = 30, 
+        bgcolor=ft.Colors.AMBER_100, 
+        width=300, height=100, 
+        alignment=ft.alignment.center, 
+        border_radius=30,
+        border=ft.border.all(3, ft.Colors.AMBER_700),
+
+    )
+    page.add(conteiner)
+    
+
+ft.app(main)
