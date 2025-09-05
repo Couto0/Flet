3e27ce66c75a388de698e4ad4f6fd8a75d30e071
Couto0 <arthurdossantos1201@gmail.com>
2025-09-05 11:17:16 -0300

    refactor: update page titles and enhance text field functionality

--- LISTAS/LISTA_1/ft.textfield/src/main.py
+++ LISTAS/LISTA_1/ft.textfield/src/main.py
@@ -4,13 +4,14 @@ import flet as ft
 def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.DARK
     page.dark_theme = ft.Theme(color_scheme_seed=ft.Colors.GREEN)
-    page.title = "Lista 1"
+    page.title = "Nivel_2"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.font = ['popins', 'times new roman', 'arial']
-    page.add(ft.Text("Bem-vindo ao IFNMG Diamantina!", size = 20, weight = ft.FontWeight.BOLD,),
-             ft.Text('Você está aprendendo a usar o Flet para criar interfaces gráficas com Python.'),
-             ft.Text('Este é um componente de Texto (ft.Text).'))
+    a = ft.TextField(label="Nome", width=300, border_color=ft.Colors.GREEN_50, text_size=20,),
+    page.add(a,
+             ft.ElevatedButton(text = 'MOSTRAR', on_click = lambda e:'txt'),
+             ft.Text(value = {a}, size = 20, weight = ft.FontWeight.BOLD,))
 
     
     
--- LISTAS/LISTA_1/ft.txt/src/main.py
+++ LISTAS/LISTA_1/ft.txt/src/main.py
@@ -4,7 +4,7 @@ import flet as ft
 def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.DARK
     page.dark_theme = ft.Theme(color_scheme_seed=ft.Colors.GREEN)
-    page.title = "Lista 1"
+    page.title = "Nivel_1"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.font = ['popins', 'times new roman', 'arial']
